f6dc601a87e6600a64227a224992e700ba14d8a1
Monk <mayankmehra667@gmail.com>
2021-08-08 14:14:32 +0530

    Initial commit

--- Monk/urls.py
+++ Monk/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.static import serve
+from django.conf.urls import url
+from Monk import settings
 
 
 admin.site.site_header = "Monk"
@@ -12,4 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
--- home/urls.py
+++ home/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views
-from django.views.static import serve
-from django.conf.urls import url
-from Monk import settings
+
+
 urlpatterns = [
     path("", views.index, name='home'),
     path("about", views.about, name='about'),
     path("services", views.services, name='services'),
     path("contact", views.contact, name='contact'),
     path('accounts/', include('allauth.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':        settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
     # path("submitcontact", views.contact, name='contact'),
   
